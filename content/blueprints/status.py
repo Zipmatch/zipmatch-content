8951f7384f2bfa9095c81b22237163274b6d42f5
Lars Hansson <lars@zipmatch.com>
2017-12-22 20:22:22 +0800

    Swagger Removal Machine

--- content/blueprints/status.py
+++ content/blueprints/status.py
@@ -6,22 +6,5 @@ blueprint = Blueprint('status', __name__)
 
 @blueprint.route('/status')
 def status():
-    """ Status endpoint
-    ---
-    tags:
-      - status
-    definitions:
-      - schema:
-          id: Status
-          description: Service status
-          properties:
-            message:
-              type: string
-              description: The status message
-    responses:
-      200:
-        description: Everything is fine and dandy
-        schema:
-          $ref: '#/definitions/Status'
-    """
+    """ Status endpoint. """
     return jsonify({'message': 'Let the Content Flow!'})
